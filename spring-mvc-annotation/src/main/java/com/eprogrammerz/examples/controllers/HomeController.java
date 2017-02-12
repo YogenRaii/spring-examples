01b77410dc63554a216ad62c9f197752f03a7008
542596 <yogen.rai@delta.com>
2017-02-12 12:24:44 -0500

    spring mvc exmaple without xml is added

---
+++ spring-mvc-annotation/src/main/java/com/eprogrammerz/examples/controllers/HomeController.java
@@ -0,0 +1,22 @@
+package com.eprogrammerz.examples.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by 542596 on 2/12/2017.
+ */
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(Model model) {
+        log.info("index()");
+        model.addAttribute("msg", "Welcom to the Spring MVC Application!!!");
+        return "index";
+    }
+}
