97ecdb529c53b001ff1f45ecdef7e6d20aea9e92
Yogen Rai <yogen.rai.992@gmail.com>
2017-10-21 15:16:57 -0500

    Created skeleton for hiring application with activiti.

---
+++ spring-boot-activiti/src/main/java/com/eprogrammerz/activiti/hiring/HiringApplication.java
@@ -0,0 +1,12 @@
+package com.eprogrammerz.activiti.hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringApplication.class, args);
+	}
+}
