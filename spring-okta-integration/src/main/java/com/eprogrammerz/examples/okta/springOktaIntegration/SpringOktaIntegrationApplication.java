4af280e91e423126f4b39f5a5851e20afe055301
Yogen Rai <yogen.rai.992@gmail.com>
2018-01-24 11:54:41 -0800

    Added okta integration with spring boot for SSO

---
+++ spring-okta-integration/src/main/java/com/eprogrammerz/examples/okta/springOktaIntegration/SpringOktaIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.eprogrammerz.examples.okta.springOktaIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOktaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOktaIntegrationApplication.class, args);
+	}
+}
---
+++ spring-okta-integration/src/main/java/com/eprogrammerz/examples/okta/springOktaIntegration/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.eprogrammerz.examples.okta.springOktaIntegration.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+
+@EnableWebSecurity
+@Component
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Value("${security.saml2.metadata-url}")
+    String metadataUrl;
+
+    @Value("${server.ssl.key-alias}")
+    String keyAlias;
+
+    @Value("${server.ssl.key-store-password}")
+    String password;
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${server.ssl.key-store}")
+    String keyStoreFilePath;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/saml/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(saml())
+                .serviceProvider()
+                .keyStore()
+                .storeFilePath("saml/keystore.jks")
+                .password(this.password)
+                .keyname(this.keyAlias)
+                .keyPassword(this.password)
+                .and()
+                .protocol("https")
+                .hostname(String.format("%s:%s", "localhost", this.port))
+                .basePath("/")
+                .and()
+                .identityProvider()
+                .metadataFilePath(this.metadataUrl);
+    }
+}
