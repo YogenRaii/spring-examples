0ca15903de73a973f45ba3bddc495b4fdfb17242
Yogen Rai <yogen.rai.992@gmail.com>
2019-04-23 20:55:15 -0500

    Removed unused imports

--- spring-boot-mongo/src/main/java/com/eprogrammerz/examples/springbootmongo/SpringBootMongoApplication.java
+++ spring-boot-mongo/src/main/java/com/eprogrammerz/examples/springbootmongo/SpringBootMongoApplication.java
@@ -1,6 +1,5 @@
 package com.eprogrammerz.examples.springbootmongo;
 
-import com.eprogrammerz.examples.springbootmongo.models.Base;
 import com.eprogrammerz.examples.springbootmongo.models.Derived;
 import com.eprogrammerz.examples.springbootmongo.repositories.DerivedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,29 +12,29 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMongoApplication implements CommandLineRunner {
-	@Autowired
-	private DerivedRepository derivedRepository;
+    @Autowired
+    private DerivedRepository derivedRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMongoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongoApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-		System.out.println("logging app...");
-		Derived derived1 = new Derived();
-		derived1.setFieldB("fieldB1");
-		derived1.setFieldA("fieldA1");
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println("logging app...");
+        Derived derived1 = new Derived();
+        derived1.setFieldB("fieldB1");
+        derived1.setFieldA("fieldA1");
 
-		Derived derived2 = new Derived();
-		derived2.setFieldB("fieldB2");
-		derived2.setFieldA("fieldA2");
-		this.derivedRepository.save(Arrays.asList(derived1, derived2));
+        Derived derived2 = new Derived();
+        derived2.setFieldB("fieldB2");
+        derived2.setFieldA("fieldA2");
+        this.derivedRepository.save(Arrays.asList(derived1, derived2));
 
-		List<Derived> deriveds = this.derivedRepository.findByFieldA("fieldA1");
-		System.out.println(deriveds);
+        List<Derived> deriveds = this.derivedRepository.findByFieldA("fieldA1");
+        System.out.println(deriveds);
 
-		List<Derived> deriveds1 = this.derivedRepository.findByFieldB("fieldB2");
-		System.out.println(deriveds1);
-	}
+        List<Derived> deriveds1 = this.derivedRepository.findByFieldB("fieldB2");
+        System.out.println(deriveds1);
+    }
 }
--- spring-boot-mongo/src/main/java/com/eprogrammerz/examples/springbootmongo/repositories/DerivedRepository.java
+++ spring-boot-mongo/src/main/java/com/eprogrammerz/examples/springbootmongo/repositories/DerivedRepository.java
@@ -1,6 +1,5 @@
 package com.eprogrammerz.examples.springbootmongo.repositories;
 
-import com.eprogrammerz.examples.springbootmongo.models.Base;
 import com.eprogrammerz.examples.springbootmongo.models.Derived;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -8,5 +7,6 @@ import java.util.List;
 
 public interface DerivedRepository extends MongoRepository<Derived, String> {
     List<Derived> findByFieldA(String fieldA);
+
     List<Derived> findByFieldB(String fieldB);
 }
